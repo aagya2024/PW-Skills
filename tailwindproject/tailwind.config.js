4bbfa27f7865fce6df1495c47bb2d694521fc25a
aagya2024 <aagyashakya2024@gmail.com>
2024-10-08 23:08:02 +0545

    Add files via upload

---
+++ tailwindproject/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        './dist/**/*.html', // Ensure Tailwind scans HTML files in the dist directory
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
